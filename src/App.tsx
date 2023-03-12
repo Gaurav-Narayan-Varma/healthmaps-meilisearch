e9eac093c80b9c76e8cb790e3d3ed59350861f5b
Gaurav Varma <gaurav.narayan.varma@gmail.com>
2023-03-11 22:14:41 -0800

    test: adding type declaration file for instancesearch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import ReactDOM from 'react-dom';
 import { useState } from 'react'
+import { InstantSearch, SearchBox, Hits, Highlight, Configure } from 'react-instantsearch-dom';
+import {instantMeiliSearch} from '@meilisearch/instant-meilisearch';
+
+const searchClient = instantMeiliSearch(
+  "http://localhost:7700"
+);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-    </div>
-  )
+    <InstantSearch indexName="movies" searchClient={searchClient}>
+      <SearchBox />
+      <Hits hitComponent={Hit} />
+      <Configure hitsPerPage={10} />
+    </InstantSearch>
+  );
 }
 
-export default App
+const Hit = (props: any) => (
+  <Highlight attribute="title" hit={props.hit} />
+);
+
+export default App;
