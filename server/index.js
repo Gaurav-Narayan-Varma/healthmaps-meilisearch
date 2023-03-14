36b54e6b7705a60f47ba7e81bea003f6195c04dc
Gaurav Varma <gaurav.narayan.varma@gmail.com>
2023-03-14 10:46:54 -0700

    set up express

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.listen(1337, () => {
+  console.log('Server started on port 1337');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+// export default app;
