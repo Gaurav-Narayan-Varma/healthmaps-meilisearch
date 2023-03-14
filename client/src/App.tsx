4729eb3920cdce7a2941354c0173be84e29f710b
Gaurav Varma <gaurav.narayan.varma@gmail.com>
2023-03-14 16:35:32 -0700

    meilisearch instance updates data from backend json file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,42 @@ import ReactDOM from 'react-dom';
 import { useState } from 'react'
 import { InstantSearch, SearchBox, Hits, Highlight, Configure } from 'react-instantsearch-dom';
 import {instantMeiliSearch} from '@meilisearch/instant-meilisearch';
+import fs from 'fs';
 
 const searchClient = instantMeiliSearch(
   "http://localhost:7700"
 );
 
+fetch('http://localhost:1337/')
+  .then(data => data.json())
+  .then(data => {
+    fetch('http://localhost:7700/indexes/meteorites/documents', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+  });
+
+// const meteoritesJson: string = `../meteorites.json`;
+
+// fetch('http://localhost:7700/indexes/meteorites/documents', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   body: meteoritesJson
+// })
+// .then(response => {
+//   console.log(response)
+// })
+// .catch(error => {
+//   console.log(error)
+// });
+
 function App() {
+  
   return (
     <InstantSearch indexName="meteorites" searchClient={searchClient}>
       <SearchBox />
