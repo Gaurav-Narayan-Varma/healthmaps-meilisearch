b33d747dd32dfe1d8909f15e8b1668489487e6d2
Gaurav Varma <gaurav.narayan.varma@gmail.com>
2023-03-16 21:51:56 -0700

    installed axios

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,28 @@
-import express from 'express';
-import cors from 'cors'
-import meteorites from '../meteorites.json' assert { type: "json" }
+import express from "express";
+import cors from "cors";
+import meteorites from "../meteorites.json" assert { type: "json" };
+import axios from "axios";
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://127.0.0.1:5173',
-}))
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5173",
+  })
+);
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(1337, () => {
-  console.log('Server started on port 1337');
+  console.log("Server started on port 1337");
 });
 
-app.get('/', (req, res) => {
-  res.header("Content-Type",'application/json');
+app.get("/", (req, res) => {
+  res.header("Content-Type", "application/json");
   res.send(meteorites);
 });
+
+// fetch("https://sample-videos.com/csv/Sample-Spreadsheet-100-rows.csv").then(
+//   (data) => console.log(data)
+// );
